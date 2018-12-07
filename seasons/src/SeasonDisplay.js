c0ad9a16cd570fe93b5ba708a424435e63205f02
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-07 17:26:54 +0000

    larn state

---
+++ seasons/src/SeasonDisplay.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const SeasonDisplay = (props)=>{
+  return <div>SeasonDisplay</div>;
+};
+
+export default SeasonDisplay;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay';
 
-const App = ()=>{
-  return <div> React App </div>
-};
+class App extends React.Component{
+  constructor(porps){
+    super(porps);
+    this.state = {lat:null,errorMessage:null};
+    
+    window.navigator.geolocation.getCurrentPosition(
+      (value)=>{
+        this.setState({lat:value.coords.latitude});
+      },
+      (err)=>{
+        this.setState({errorMessage:err.message});
+      }
+    );
+  }
+  
+  render() {
+    if(!this.state.lat && this.state.errorMessage){
+      return <div>{this.state.errorMessage}</div>;
+    } else if(this.state.lat && !this.state.message){
+      return <SeasonDisplay/>;
+    } else{
+      return <div>Loading...</div>;
+    }
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
