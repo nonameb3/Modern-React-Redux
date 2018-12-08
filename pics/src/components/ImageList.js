7c9687cc004bb5c6de09ac9e6ac299b0270161e4
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-08 17:57:29 +0000

    finish pics ver2

--- pics/src/components/ImageList.js
+++ pics/src/components/ImageList.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './ImageList.css';
+import ImageCard from './ImageCard';
 
-const ImageList = (prop)=> {
-  const images = prop.images.map((image)=>{
-    return <img key={image.id} alt={image.description} src={image.urls.regular} />;
+const ImageList = (props)=> {
+  const images = props.images.map((image)=>{
+    return <ImageCard key={image.id} image={image} />;
   });
-  return <div>{images}</div>;
+  
+  return (
+    <div className="image-list">
+      {images}
+    </div>
+  );
 };
 
 export default ImageList;
