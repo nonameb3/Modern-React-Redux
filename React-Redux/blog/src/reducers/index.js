f055304b36965aa03c5a65cbd7402c442953904f
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-13 22:19:13 +0700

    fix code

--- React-Redux/blog/src/reducers/index.js
+++ React-Redux/blog/src/reducers/index.js
@@ -1,5 +1,10 @@
 import {combineReducers} from 'redux';
 
-export default combineReducers({
-    dummy:()=>'dummy message'
+export default combineReducers((state = null , action) =>{
+  switch (action.type) {
+    case 'FETCH_POSTS':
+      return action.payload;
+    default:
+      return state;
+  }
 });
