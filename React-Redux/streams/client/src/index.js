2415625a3ee18fd090ef79953a6a823a4199d77f
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-16 23:23:54 +0700

    add config for redux devtools

--- React-Redux/streams/client/src/index.js
+++ React-Redux/streams/client/src/index.js
@@ -2,13 +2,14 @@
 require('dotenv').config()
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 
 import App from './components/App';
 import Reducer from './reducers';
 
-const store = createStore(Reducer);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(Reducer, composeEnhancers(applyMiddleware()));
 
 ReactDOM.render(
   <Provider store={store}>
