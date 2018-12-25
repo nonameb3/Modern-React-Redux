33f426d01feb7968eca9ba1ad0f3cec3439d54cb
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-26 00:42:44 +0700

    add obs, flv.js

--- React-Redux/streams/client/src/components/streams/streamShow.js
+++ React-Redux/streams/client/src/components/streams/streamShow.js
@@ -1,24 +1,39 @@
 import React, { Component } from 'react';
 import { getStream } from '../../actions';
 import { connect } from 'react-redux';
+import flv from 'flv.js';
 
 export class streamShow extends Component {
+  constructor(props){
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
   componentDidMount(){
-    this.props.getStream(this.props.match.params.id);
+    const {id} = this.props.match.params;
+    this.props.getStream(id);
+    this.player = flv.createPlayer({
+      type: 'flv',
+      url: `http://localhost:8000/live/${id}.flv`
+    });
+
+    this.player.attachMediaElement(this.videoRef.current);
+    this.player.load();
   }
 
   render() {
     if(!this.props.stream){
-      return <div>Loading...</div>
+      return <div>Loading...</div>;
     }
 
     const {title, description} = this.props.stream;
     return (
       <div>
+        <video ref={this.videoRef} style={{ width:'100%'}} controls />
         <h1>{title}</h1>
         <h4>{description}</h4>
       </div>
-    )
+    );
   }
 }
 
