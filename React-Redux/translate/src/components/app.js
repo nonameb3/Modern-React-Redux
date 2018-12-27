fedebea6c23fac93b6b9b8d47759ccb25fcfb388
Waraphon Roonnapai <eepw0211@gmail.com>
2018-12-27 23:50:30 +0700

    add translate pj

---
+++ React-Redux/translate/src/components/app.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class app extends Component {
+  render() {
+    return (
+      <div className="ui container">
+        App
+      </div>
+    );
+  }
+}
+
+export default app;
