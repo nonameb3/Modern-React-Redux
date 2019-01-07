528bec4bf7b91123df268915449754602ead240b
waraphon roonnapai <eepw0211@gmail.com>
2019-01-07 13:09:01 +0700

    add useEffect

--- React/hooks-simple/src/components/ResourceList.js
+++ React/hooks-simple/src/components/ResourceList.js
@@ -1,21 +1,55 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-export class ResourceList extends Component {
-  state = {resource : []}
+// use useState, useEddect Hooks
+const ResourceList = ({resource}) => {
+  // set state
+  const [resources, setResource] = useState([]);
 
-  async componentDidMount(){
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
-    this.setState({resource:response.data});
-  }
+  const fectResource = async (resource) => {
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`);
+    setResource(response.data);
+  };
+  
+  // same as componentDidMount , Update
+  useEffect(
+    ()=>{
+      fectResource(resource);
+    },[resource] // only run when userId changed
+  );
 
-  render() {
-    return (
-      <div>
-        {this.state.resource.length}
-      </div>
-    );
-  }
-}
+
+  return (
+    <div>
+      {resources.length}
+    </div>
+  );
+  
+};
 
 export default ResourceList;
+
+// Class React
+// export class ResourceList extends React.Component {
+//   state = {resource : []}
+
+//   async componentDidMount(){
+//     const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+//     this.setState({resource:response.data});
+//   }
+
+//   async componentDidUpdate(prevPorps){
+//     if(prevPorps !== this.props){
+//       const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+//       this.setState({resource:response.data});
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <div>
+//         {this.state.resource.length}
+//       </div>
+//     );
+//   }
+// }
